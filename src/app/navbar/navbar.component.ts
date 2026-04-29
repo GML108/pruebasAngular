8eb6dc8e819d655d68730ce38dcf76f49bb65ed8
GerMLDev <gerardotalleragencia@gmail.com>
2026-04-29 13:02:06 +0200

    v01

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgIf, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
+dropdown:string | null=null;
+
+  desplegable(menu : string) {
+
+    if(this.dropdown==menu){
+  this.dropdown = null;
+    }
+    else{
+    this.dropdown = menu;
+  }
 }
+}
+
+
